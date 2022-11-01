52df630b0a133fd2bcba728f55b6934d7ab143f0
Kim <nicedarrow12@gmail.com>
2022-10-31 21:11:48 -0300

    ✨ feat: faq now really works

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-import "./styles/css/global.css"
+import "./styles/css/global.css";
+
+const questions = document.querySelectorAll(".faq__item");
+
+questions.forEach( (question) => {
+  const btn = question.querySelector('.faq__question-button');
+
+  btn.addEventListener("click", () => {
+    questions.forEach( (item) => {
+      if (item !== question) {
+        item.classList.remove("active");
+      }
+    });
+
+    question.classList.toggle("active");
+  })
+})
